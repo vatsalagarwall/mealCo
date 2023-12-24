d1b43cf0b60fd82b459a9c66504aeee979832941
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-24 17:24:29 +0530

    commit

--- client/src/Components/Success.jsx
+++ client/src/Components/Success.jsx
@@ -5,7 +5,7 @@ function Success() {
   return (
     <div className="success-container">
       <div className="success">
-        Order placed .
+        Order placed successfully.
         <div style={{ marginTop: "20px" }}>
           <Link to="/your-orders" className="btn btn-outline-dark card-button">
             My Orders
